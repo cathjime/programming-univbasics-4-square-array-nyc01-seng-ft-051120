a30c432b13acd87bec8d759b5cbe2a89ce9fd8a1
Catherine Jimenez <xcatherine.jimenez@gmail.com>
2020-04-30 14:38:50 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -5,7 +5,7 @@ def square_array(array)
   count = 0 
   
   while count < array.length do
-    new_array.push(array[count] ** array[count])
+    new_array.push(square(array[count]))
     count += 1 
   end 
   new_array
